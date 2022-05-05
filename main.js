752513f97ffbf44a4a5e9e7f689b400f7fb5dc10
Matty Gerlach <mattygerlach33@gmail.com>
2022-05-05 19:30:49 -0400

    Add simple UI to the rps game for TOP in the rps-ui branch

--- main.js
+++ main.js
@@ -1,64 +1,73 @@
-// Create variables for player and computer score
-let playerScore = 0;
-let computerScore = 0;
-
 // Create array to include the options for the game
 const options = ["rock", "paper", "scissors"];
 
+// Create variables the player and computer score and the buttons
+let playerScore = 1;
+let computerScore = 1;
+const buttons = document.querySelectorAll(".element-button");
+
+// Add an event listener to each button that calls the playRound function
+// with the playerSelection being the value of the button that is clicked
+// Create element to display after player or computer reaches five wins
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+playRound(button.value);
+if (playerScore == 6) {
+    const winner = document.createElement("div");
+    winner.classList.add("winner");
+    winner.style.color = "green";
+    winner.textContent = "Player won five rounds: Player wins!!!";
+    container.appendChild(winner);
+} else if (computerScore == 6) {
+    const winner = document.createElement("div");
+    winner.classList.add("winner");
+    winner.style.color = "red";
+    winner.textContent = "The robot won five rounds: You lost!!!";
+    container.appendChild(winner);
+} 
+});
+});
+
 // Create function for the computer "player" that will randomly return one of the three options 
 function computerPlay() {
-return options[Math.floor(Math.random(options) * 3)];
+    return options[Math.floor(Math.random(options) * 3)];
 } 
 
-// Create function for the playerChoice to contain an input box where the player can enter one of 
-// the options and have that option returned unless the input is not one of the options
-function playerChoice() { 
-    let input = prompt("Type rock, paper, or scissors");
-    if (input !== "rock" && input !== "paper" && input !== "scissors") {
-        return playerChoice();
-    } else {
-        return input;
-    }
-} 
-
-// Create function to play one round with the playerSelection and computerSelection parameters
+// Create function to play one round with the playerSelection parameter
 // Make it return a string that declares the winner
-function playRound() {
-    const playerSelection = playerChoice();
+// Turn the console.log statements into DOM methods
+function playRound(playerSelection) { 
     const computerSelection = computerPlay();
     // Player wins
     if (playerSelection === "rock" && computerSelection === "scissors" ||
     playerSelection === "paper" && computerSelection === "rock" ||
     playerSelection === "scissors" && computerSelection === "paper") {
-        console.log(`${playerSelection} vs ${computerSelection} = Player wins!`);
+        const playerWin = document.createElement('div');
+        playerWin.classList.add("playerWin");
+        playerWin.textContent = `${playerSelection} vs ${computerSelection} = Player wins: ${playerScore}`;
+        container.appendChild(playerWin);
         playerScore++
     // Computer wins
     } else if (playerSelection === "rock" && computerSelection === "paper" ||
     playerSelection === "paper" && computerSelection === "scissors" ||
     playerSelection === "scissors" && computerSelection === "rock") {
-        console.log(`${playerSelection} vs ${computerSelection} = Computer wins!`);
+        const computerWin = document.createElement('div');
+        computerWin.classList.add("computerWin");
+        computerWin.textContent = `${playerSelection} vs ${computerSelection} = Computer wins: ${computerScore}`;
+        container.appendChild(computerWin);
         computerScore++
         // Tie game
     } else {
-        console.log(`${playerSelection} vs ${computerSelection} = Tie!`);
+        const tie = document.createElement('div');
+        tie.classList.add("tie");
+        tie.textContent = `${playerSelection} vs ${computerSelection} = Tie!`;
+        container.appendChild(tie);
     }
-}  
+} 
 
-// Create function to play 5 rounds and report the final result at the end
-function game() {
-    for (let i = 0; i < 5; i++) {
-        playRound();
-        }
-        finalResult(); 
-    } console.log(game());
+ 
 
-// Create function to display the result of the 5 rounds
-    function finalResult() {
-        if (playerScore > computerScore) {
-            console.log("Player won this round!");
-        } else if (playerScore < computerScore) {
-            console.log("Computer won this round!");
-        } else {
-            console.log("This round was a tie!");
-        }
-    }
+
+
+
+
